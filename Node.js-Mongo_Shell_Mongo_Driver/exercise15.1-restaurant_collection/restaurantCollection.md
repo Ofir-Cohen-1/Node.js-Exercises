63cbf088959b2e130bd1a06be65a015ebc9f8a34
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-06-20 01:03:58 +0300

    Added Exercises 15.1 and 15.2

---
+++ Node.js-Mongo_Shell_Mongo_Driver/exercise15.1-restaurant_collection/restaurantCollection.md
@@ -0,0 +1,10 @@
+## Commands:
+
+-   create DB:
+    `use findMyRestaurant`
+
+-   create collection:
+    `db.createCollection("restaurants")`
+
+-   create documents:
+    `db.restaurants.insertMany(restaurant[])`
---
+++ Node.js-Mongo_Shell_Mongo_Driver/exercise15.1-restaurant_collection/restaurants.js
@@ -0,0 +1,122 @@
+const restaurants = [
+  {
+    name: "Space",
+    address: {
+      city: "Emunim",
+      street: "Ha-Zayit",
+      coordinates: [-77.564, 40.677],
+    },
+    cuisineType: "Meets",
+    kosher: true,
+    reviews: [
+      {
+        date: "11.2.22",
+        score: 5,
+      },
+      {
+        date: "12.2.22",
+        score: 4,
+      },
+      {
+        date: "13.2.22",
+        score: 5,
+      },
+    ],
+  },
+  {
+    name: "Ratatui",
+    address: {
+      city: "Emunim",
+      street: "Ha-Zayit",
+      coordinates: [-72.164, 20.677],
+    },
+    cuisineType: "Dairy",
+    kosher: true,
+    reviews: [
+      {
+        date: "31.2.22",
+        score: 5,
+      },
+      {
+        date: "10.2.22",
+        score: 2,
+      },
+      {
+        date: "30.2.22",
+        score: 3,
+      },
+    ],
+  },
+  {
+    name: "Aroma",
+    address: {
+      city: "Eilat",
+      street: "Ha-Satat",
+      coordinates: [-17.564, -40.677],
+    },
+    cuisineType: "Cafe",
+    kosher: true,
+    reviews: [
+      {
+        date: "21.2.22",
+        score: 5,
+      },
+      {
+        date: "22.2.22",
+        score: 4,
+      },
+      {
+        date: "23.2.22",
+        score: 5,
+      },
+    ],
+  },
+  {
+    name: "japanika",
+    address: {
+      city: "Rehovot",
+      street: "Haim Pekris",
+      coordinates: [-74.564, 40.797],
+    },
+    cuisineType: "Asia",
+    kosher: true,
+    reviews: [
+      {
+        date: "18.2.22",
+        score: 5,
+      },
+      {
+        date: "19.2.22",
+        score: 1,
+      },
+      {
+        date: "20.2.22",
+        score: 3,
+      },
+    ],
+  },
+  {
+    name: "Cafe_Market",
+    address: {
+      city: "Jerusalem",
+      street: "HaAfarsek",
+      coordinates: [-47.564, 20.677],
+    },
+    cuisineType: "Cafe",
+    kosher: false,
+    reviews: [
+      {
+        date: "14.2.22",
+        score: 5,
+      },
+      {
+        date: "15.2.22",
+        score: 5,
+      },
+      {
+        date: "16.2.22",
+        score: 5,
+      },
+    ],
+  },
+];
